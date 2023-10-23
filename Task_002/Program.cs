b352c9eceb57aa154e1ca8f7bd481f38b61b99f6
Ирина <ira.sheina.92@gmail.com>
2023-10-23 08:58:09 +0300

    добавлено решение Task_001

---
+++ Task_002/Program.cs
@@ -0,0 +1,45 @@
+﻿//Напишите программу, которая находит сумму элементов 
+//c нечетными индексами в одномерном массиве целых чисел и выводит результат на экран.
+
+
+int[] array = CreateRandomarray();
+ShowArray(array);
+int sum = SumOfOddIndexElements(array);
+Console.WriteLine(sum);
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+        if (i < array.Length - 1) // Проверяем, не последний ли элемент
+        {
+        Console.Write("\t");
+        }
+    }
+    Console.WriteLine();
+}
+
+int [] CreateRandomarray()
+{
+    int minSizeArray = 0;
+    int maxSizeArray = 100;
+    Random rnd = new Random();
+    int[] rndArr = new int [8];
+    for (int i = 0; i < rndArr.Length; i++)
+    {
+        rndArr[i] = rnd.Next(minSizeArray, maxSizeArray);
+    }
+    return rndArr;
+}
+
+int SumOfOddIndexElements(int[] array)
+{	
+    int sum = 0;
+    for ( int i = 1; i < array.Length; i+=2) //i=i+2
+    {
+        sum = sum + array[i]; 
+    }
+    return sum;
+
+}
